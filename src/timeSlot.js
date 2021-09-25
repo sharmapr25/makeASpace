1faa4b15456710b876314b0256deab56159a85be
preeti@coffeebeans.io <preetisharma@preetis-MacBook-Pro.local>
2021-09-25 11:59:31 +0530

    Written test for overlapping timeslot in timeslot.js

--- src/timeSlot.js
+++ src/timeSlot.js
@@ -1,16 +1,37 @@
 const InvalidTimeSlotInputError = require('./error/invalidTimeSlotInputError');
 const Time = require('./time');
 
-class TimeSlot{
-  constructor(startDate, endDate){
+class TimeSlot {
+  constructor(startDate, endDate) {
     this._startDate = startDate;
     this._endDate = endDate;
   }
 
-  static create(startInput, endInput){
+  _isOverlappingWithStartDate(anotherTimeSlot) {
+    return (
+      anotherTimeSlot._startDate.isLessThan(this._startDate) &&
+      anotherTimeSlot._endDate.isGreaterThan(this._startDate)
+    );
+  }
+
+  _isOverlappingWithEndDate(anotherTimeSlot) {
+    return (
+      anotherTimeSlot._startDate.isGreaterThan(this._startDate) &&
+      anotherTimeSlot._startDate.isLessThan(this._endDate)
+    );
+  }
+
+  isOverlappingWith(anotherTimeSlot) {
+    return (
+      this._isOverlappingWithStartDate(anotherTimeSlot) ||
+      this._isOverlappingWithEndDate(anotherTimeSlot) || anotherTimeSlot._startDate.isSame(this._startDate)
+    );
+  }
+
+  static create(startInput, endInput) {
     const startTime = Time.create(startInput);
     const endTime = Time.create(endInput);
-    if(startTime.isGreaterThan(endTime)){
+    if (startTime.isGreaterThan(endTime)) {
       throw new InvalidTimeSlotInputError();
     }
     return new TimeSlot(startTime, endTime);
--- test/timeSlot.test.js
+++ test/timeSlot.test.js
@@ -19,3 +19,55 @@ describe('create', () => {
     expect(error).toThrow(InvalidTimeSlotInputError);
   });
 });
+
+describe('isOverlappingWith', () => {
+  it("should return true when given two time slots are not same", () => {
+    const timeslot = TimeSlot.create("10:00", "12:00");
+
+    expect(timeslot.isOverlappingWith(timeslot)).toBeTruthy();
+  });
+
+  it("should return true when given time slot is 11hr to 12hr that overlap with 10hr to 12hr", () => {
+    const tenHoursToTweleveHours = TimeSlot.create("10:00", "12:00");
+    const elevenHoursToTweleveHours = TimeSlot.create("11:00", "12:00");
+
+    expect(tenHoursToTweleveHours.isOverlappingWith(elevenHoursToTweleveHours)).toBeTruthy();
+  });
+
+  it("should return true when given time slot is 9hr to 12hr that overlap with 10hr to 12hr", () => {
+    const tenHoursToTweleveHours = TimeSlot.create("10:00", "12:00");
+    const nineHoursToTweleveHours = TimeSlot.create("9:00", "12:00");
+
+    expect(
+      tenHoursToTweleveHours.isOverlappingWith(nineHoursToTweleveHours)
+    ).toBeTruthy();
+  });
+
+ it("should return true when given time slot is 9hr to 12hr that overlap with 10hr to 12hr", () => {
+   const tenHoursToTweleveHours = TimeSlot.create("10:00", "12:00");
+   const nineHoursToTweleveHours = TimeSlot.create("9:00", "12:00");
+
+   expect(
+     tenHoursToTweleveHours.isOverlappingWith(nineHoursToTweleveHours)
+   ).toBeTruthy();
+ });
+
+ it("should return false when given time slot is 12hr to 13hr that not overlapping with 10hr to 12hr", () => {
+   const tenHoursToTweleveHours = TimeSlot.create("10:00", "12:00");
+   const tweleveHoursToThirteenHours = TimeSlot.create("12:00", "13:00");
+
+   expect(
+     tenHoursToTweleveHours.isOverlappingWith(tweleveHoursToThirteenHours)
+   ).toBeFalsy();
+ });
+
+ it("should return false when given time slot is 9hr to 10hr that not overlapping with 10hr to 12hr", () => {
+   const tenHoursToTweleveHours = TimeSlot.create("10:00", "12:00");
+   const nineHoursToTenHours = TimeSlot.create("9:00", "10:00");
+
+   expect(
+     tenHoursToTweleveHours.isOverlappingWith(nineHoursToTenHours)
+   ).toBeFalsy();
+ });
+
+});
